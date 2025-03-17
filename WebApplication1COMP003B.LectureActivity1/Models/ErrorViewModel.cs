3e4a838db8d97cfa324dcb0389f41a53e5ca5fb5
flores-97 <floresvictor15@gmail.com>
2025-03-17 10:56:11 -0800

    Add project files.

---
+++ WebApplication1COMP003B.LectureActivity1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApplication1COMP003B.LectureActivity1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
